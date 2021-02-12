c286bbd8ac086a5629cff347733f72e0c8f80500
arachnothrone <arachnothrone@gmail.com>
2021-02-12 00:10:10 -0500

    Querying every 5 minutes, write to a file

--- wserver.c
+++ wserver.c
@@ -15,14 +15,18 @@
 #include <bluetooth/rfcomm.h>
 
 #define HC6ADDRESS  "00:20:10:08:FB:8A"
+#define MAIN_CYCLE_PERIOD_SEC   (300)   
 
 int main(int argc, char **argv)
 {
+    static int run_server = 1;
     long    time_millis;
     long    time_millis_prev;
     time_t  time_seconds;
     time_t  time_seconds_prev;
     struct  timespec timestamp;
+    long    currentTimeMs;
+    long    previousTimeMs;
     
     // Scan devices init
     inquiry_info* devices = NULL;
@@ -39,6 +43,9 @@ int main(int argc, char **argv)
     int bytes_read;
     char readBuffer[200] = {0};
 
+    FILE* pDataLogFile;
+    pDataLogFile = fopen("datalog.txt", "a+");
+    
     clock_gettime(CLOCK_MONOTONIC, &timestamp);       // CLOCK_REALTIME
     time_seconds_prev = timestamp.tv_sec;
     time_millis_prev = round(timestamp.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
@@ -47,6 +54,7 @@ int main(int argc, char **argv)
         time_millis_prev = 0;
     }
     printf("TS: %ld.%ld s\n", time_seconds_prev, time_millis_prev);
+    printf("size of long = %ld\n", sizeof(long));
 
     // dev_id = hci_get_route(NULL);
     // sock = hci_open_dev( dev_id );
@@ -75,72 +83,95 @@ int main(int argc, char **argv)
     // free(devices);
     // close(sock);
 
-    // RFCOMM client
-    // allocate socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    clock_gettime(CLOCK_MONOTONIC, &timestamp);
+    previousTimeMs = timestamp.tv_sec; // * 1000 + timestamp.tv_nsec / 1000;
+    printf("---> previousTimeMs (sec) = %ld\n", previousTimeMs);
 
-    // Set the connection parameters
-    address.rc_family = AF_BLUETOOTH;
-    address.rc_channel = 1;
-    str2ba(dest, &address.rc_bdaddr);
-
-    // connect to server
-    printf("Connecting to HC-06...\n");
-    status = connect(s, (struct sockaddr*)&address, sizeof(address));
-    
-    clock_gettime(CLOCK_MONOTONIC, &timestamp);       // CLOCK_REALTIME
-    time_seconds_prev = timestamp.tv_sec;
-    time_millis_prev = round(timestamp.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
-    if (time_millis_prev > 999) {
-        time_seconds_prev ++;
-        time_millis_prev = 0;
-    }
-    printf("TS: %ld.%ld s\n", time_seconds_prev, time_millis_prev);
-
-    // send message
-    if (0 == status)
+    while (run_server)
     {
-        printf("Successful.\n");
-        printf("Requesting data from HC-06...\n");
-        status = send(s, "---> wserver data request", 43, 0);
-    }
+        clock_gettime(CLOCK_MONOTONIC, &timestamp);
+        currentTimeMs = timestamp.tv_sec; // * 1000 + timestamp.tv_nsec / 1000;
 
-    clock_gettime(CLOCK_MONOTONIC, &timestamp);       // CLOCK_REALTIME
-    time_seconds_prev = timestamp.tv_sec;
-    time_millis_prev = round(timestamp.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
-    if (time_millis_prev > 999) {
-        time_seconds_prev ++;
-        time_millis_prev = 0;
-    }
-    printf("TS: %ld.%ld s\n", time_seconds_prev, time_millis_prev);
-
-    if (status >= 0)
-    {
-        // receive data
-        printf("... waiting ...\n");
-        bytes_read = recv(s, readBuffer, sizeof(readBuffer), 0);
-        printf("received packet\n");
-        if (bytes_read > 0)
+        if (currentTimeMs - previousTimeMs > MAIN_CYCLE_PERIOD_SEC)
         {
-            printf("Received data: %s\n", readBuffer);
+            printf("   ===> currentTimeMs (sec) = %ld\n", currentTimeMs);
+            
+            // RFCOMM client
+            // allocate socket
+            s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+            // Set the connection parameters
+            address.rc_family = AF_BLUETOOTH;
+            address.rc_channel = 1;
+            str2ba(dest, &address.rc_bdaddr);
+
+            // connect to server
+            printf("Connecting to HC-06...\n");
+            status = connect(s, (struct sockaddr*)&address, sizeof(address));
+            
+            clock_gettime(CLOCK_MONOTONIC, &timestamp);       // CLOCK_REALTIME
+            time_seconds_prev = timestamp.tv_sec;
+            time_millis_prev = round(timestamp.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
+            if (time_millis_prev > 999) {
+                time_seconds_prev ++;
+                time_millis_prev = 0;
+            }
+            printf("TS: %ld.%ld s\n", time_seconds_prev, time_millis_prev);
+
+            // send message
+            if (0 == status)
+            {
+                printf("Successful.\n");
+                printf("Requesting data from HC-06...\n");
+                status = send(s, "---> wserver data request", 43, 0);
+            }
+
+            clock_gettime(CLOCK_MONOTONIC, &timestamp);       // CLOCK_REALTIME
+            time_seconds_prev = timestamp.tv_sec;
+            time_millis_prev = round(timestamp.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
+            if (time_millis_prev > 999) {
+                time_seconds_prev ++;
+                time_millis_prev = 0;
+            }
+            printf("TS: %ld.%ld s\n", time_seconds_prev, time_millis_prev);
+
+            if (status >= 0)
+            {
+                // receive data
+                printf("... waiting ...\n");
+                bytes_read = recv(s, readBuffer, sizeof(readBuffer), 0);
+                printf("received packet\n");
+                if (bytes_read > 0)
+                {
+                    printf("Received data: %s\n", readBuffer);
+                    fprintf(pDataLogFile, "ARD1:: %s", readBuffer);     // CR is already in the received buffer
+                    fflush(pDataLogFile);
+                }
+            }
+            else
+            {
+                perror("Can't connect to server");
+            }
+
+            clock_gettime(CLOCK_MONOTONIC, &timestamp);       // CLOCK_REALTIME
+            time_seconds_prev = timestamp.tv_sec;
+            time_millis_prev = round(timestamp.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
+            if (time_millis_prev > 999) {
+                time_seconds_prev ++;
+                time_millis_prev = 0;
+            }
+            printf("TS: %ld.%ld s\n", time_seconds_prev, time_millis_prev);
+
+            printf("Close connection.\n");
+            close(s);
+
+            clock_gettime(CLOCK_MONOTONIC, &timestamp);
+            previousTimeMs = timestamp.tv_sec;
+            //previousTimeMs = currentTimeMs;
         }
     }
-    else
-    {
-        perror("Can't connect to server");
-    }
 
-    clock_gettime(CLOCK_MONOTONIC, &timestamp);       // CLOCK_REALTIME
-    time_seconds_prev = timestamp.tv_sec;
-    time_millis_prev = round(timestamp.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
-    if (time_millis_prev > 999) {
-        time_seconds_prev ++;
-        time_millis_prev = 0;
-    }
-    printf("TS: %ld.%ld s\n", time_seconds_prev, time_millis_prev);
-
-    printf("Close connection.\n");
-    close(s);
+    fclose(pDataLogFile);
 
     return 0;
 }
